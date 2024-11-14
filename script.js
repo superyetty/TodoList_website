c96d526d45c0b7d31436760677b3bc8872c9ceaf
Afolabi Yetunde <133240718+superyetty@users.noreply.github.com>
2024-11-14 01:16:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const todoList = [
+  {
+    name: "cook dinner",
+    dueDate: "22/11/2024",
+  },
+];
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = "";
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const name = todoObject.name;
+    const dueDate = todoObject.dueDate;
+    console.log(dueDate);
+
+    const html = `
+      <div class>${name}</div>
+      <div>${dueDate}</div>
+      <button class="js-todo-delete delete-button" 
+      onclick="todoList.splice(${i}, 1)
+      console.log('deleted')
+      renderTodoList()
+      ">
+        Delete
+      </button>
+    `;
+    todoListHTML += html;
+  }
+  console.log(todoListHTML);
+  document.querySelector(".js-todo-display").innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  console.log("clicked");
+  const inputEl = document.querySelector(".js-input");
+  const name = inputEl.value;
+
+  const dateEl = document.querySelector(".js-date-input");
+  const dueDate = dateEl.value;
+
+  todoList.push({
+    name: name,
+    dueDate: dueDate,
+  });
+  console.log(todoList);
+
+  renderTodoList();
+
+  inputEl.value = "";
+  dateEl.value = "";
+}
